6a07be908cf55966c236aee994a3cc9fe0757ce5
chongkessob <https://github.com/chongkessob>
2025-03-15 20:49:04 +0800

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_X6532.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_X6532-user \
+    lineage_X6532-userdebug \
+    lineage_X6532-eng
